38b5faf14f585b0a4bf19a79dc8f81fbdd5f3acc
furstinvia <furstinaptr@gmail.com>
2024-10-28 23:08:26 +0700

    update vuee

--- admin-dashboard/src/main.js
+++ admin-dashboard/src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+import ArgonDashboard from "./argon-dashboard";
 
-createApp(App).mount('#app')
+const appInstance = createApp(App);
+appInstance.use(store);
+appInstance.use(router);
+appInstance.use(ArgonDashboard);
+appInstance.mount("#app");
